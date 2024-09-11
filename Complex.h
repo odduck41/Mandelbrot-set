21c91508743e7bba67912cdb76e825d6ca0a71f1
odduck41 <GD.cats.creators@gmail.com>
2024-09-11 11:27:32 +0300

    feature

---
+++ Complex.h
@@ -0,0 +1,33 @@
+#pragma once
+
+class Complex {
+  public:
+    Complex() = default;
+    Complex(const Complex&);
+    explicit Complex(const long long&, const long long&);
+    Complex(Complex&&) noexcept;
+
+    Complex& operator=(Complex) noexcept;
+
+    Complex& operator+=(const Complex&);
+    Complex& operator-=(const Complex&);
+    Complex& operator*=(const Complex&);
+
+    Complex operator-() const;
+
+    Complex operator+(Complex) const;
+    Complex operator-(Complex) const;
+    Complex operator*(Complex) const;
+
+    bool operator==(const Complex&) const;
+
+    [[nodiscard]] long long getA() const;
+    [[nodiscard]] long long getB() const;
+
+    friend void swap(Complex&, Complex&) noexcept;
+    friend long long abs(const Complex&);
+  private:
+    long long* a_{};
+    long long* b_{};
+};
+
