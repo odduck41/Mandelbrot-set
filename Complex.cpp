78f84422b8ca20e96c8dd352552313b3bfb42828
odduck41 <GD.cats.creators@gmail.com>
2024-09-14 01:14:32 +0300

    faster

--- Complex.cpp
+++ Complex.cpp
@@ -1,57 +1,57 @@
 #include "Complex.h"
 #include <utility>
 
-void swap(Complex& a, Complex& b) noexcept {
+inline void swap(Complex& a, Complex& b) noexcept {
     std::swap(a.a_, b.a_);
     std::swap(a.b_, b.b_);
 }
 
-long double abs(const Complex& a) {
+inline long double abs(const Complex& a) {
     return a.getA() * a.getA() + a.getB() * a.getB();
 }
 
 
-ComplexPart ComplexPart::operator-() const {
+inline ComplexPart ComplexPart::operator-() const {
     return ComplexPart{-this->b_};
 }
 
-ComplexPart operator ""_i(const unsigned long long b_) {
+inline ComplexPart operator ""_i(const unsigned long long b_) {
     return ComplexPart{static_cast<long double>(b_)};
 }
 
 
-Complex::Complex(const long double& a, const long double& b)
+inline Complex::Complex(const long double& a, const long double& b)
 : a_(a), b_(b) {}
 
-Complex::Complex(const long double& a): a_(a), b_(0) {}
+inline Complex::Complex(const long double& a): a_(a), b_(0) {}
 
-Complex::Complex(const ComplexPart& b): a_(0), b_(b.b_) {}
+inline Complex::Complex(const ComplexPart& b): a_(0), b_(b.b_) {}
 
-Complex::Complex(Complex&& other) noexcept
+inline Complex::Complex(Complex&& other) noexcept
 : a_(std::exchange(other.a_, 0)),
 b_(std::exchange(other.b_, 0)) {}
 
-Complex::Complex(const Complex& other) = default;
+inline Complex::Complex(const Complex& other) = default;
 
-Complex& Complex::operator=(Complex other) noexcept {
+inline Complex& Complex::operator=(Complex other) noexcept {
     swap(*this, other);
     return *this;
 }
 
 
-Complex& Complex::operator+=(const Complex& other) {
+inline Complex& Complex::operator+=(const Complex& other) {
     this->a_ += other.a_;
     this->b_ += other.b_;
     return *this;
 }
 
-Complex& Complex::operator-=(const Complex& other) {
+inline Complex& Complex::operator-=(const Complex& other) {
     this->a_ -= other.a_;
     this->b_ -= other.b_;
     return *this;
 }
 
-Complex& Complex::operator*=(const Complex& other) {
+inline Complex& Complex::operator*=(const Complex& other) {
     const auto ta = this->a_;
     const auto tb = this->b_;
 
@@ -65,50 +65,50 @@ Complex& Complex::operator*=(const Complex& other) {
 }
 
 
-Complex Complex::operator-() const {
+inline Complex Complex::operator-() const {
     const Complex reverse(-this->a_, -this->b_);
     return reverse;
 }
 
-Complex Complex::operator+(Complex other) const {
+inline Complex Complex::operator+(Complex other) const {
     other += *this;
     return other;
 }
 
-Complex Complex::operator-(Complex other) const {
+inline Complex Complex::operator-(Complex other) const {
     return -(other-=*this);
 }
 
-Complex Complex::operator*(Complex other) const {
+inline Complex Complex::operator*(Complex other) const {
     return other *= *this;
 }
 
 
-bool Complex::operator==(const Complex& other) const {
+inline bool Complex::operator==(const Complex& other) const {
     return (this->a_ == other.a_) && (this->b_ == other.b_);
 }
 
-bool Complex::operator>(const Complex& other) const {
+inline bool Complex::operator>(const Complex& other) const {
     return abs(*this) > abs(other);
 }
 
-bool Complex::operator<(const Complex& other) const {
+inline bool Complex::operator<(const Complex& other) const {
     return abs(*this) < abs(other);
 }
 
-bool Complex::operator>=(const Complex& other) const {
+inline bool Complex::operator>=(const Complex& other) const {
     return *this > other || *this == other;
 }
 
-bool Complex::operator<=(const Complex& other) const {
+inline bool Complex::operator<=(const Complex& other) const {
     return *this < other || *this == other;
 }
 
 
-long double Complex::getA() const {
+inline long double Complex::getA() const {
     return this->a_;
 }
 
-long double Complex::getB() const {
+inline long double Complex::getB() const {
     return this->b_;
 }
