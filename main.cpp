75cddee863972b0cede31131a9444603a95fd7f4
odduck41 <GD.cats.creators@gmail.com>
2024-09-14 01:22:10 +0300

    faster, stronger..

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include "Complex.h"
 
 #define STEPS 256
+#define WIDTH (long long)1500
+#define HEIGHT (long long)1500
 
 typedef sf::Vector2<double> Vector2d;
 
@@ -26,10 +28,10 @@ inline long long step(const Complex& C) {
     return steps;
 }
 
-void generatePart(sf::Image& image, const long long startY, const long long height, const long long width,
+void generatePart(sf::Image& image, const long long startY, const long long height,
     const double& one, const Vector2d& center) {
     for (long long y = startY; y < startY + height; ++y) {
-        for (long long x = 0; x < width; ++x) {
+        for (long long x = 0; x < WIDTH; ++x) {
             Complex C(
                         (static_cast<double>(x) - center.x) / one,
                         (static_cast<double>(y) - center.y) / one
@@ -47,27 +49,30 @@ void generatePart(sf::Image& image, const long long startY, const long long heig
 }
 
 int main() {
-    const auto numThreads = std::thread::hardware_concurrency();
+    constexpr auto numThreads = std::thread::hardware_concurrency();
+    sf::RenderWindow window({WIDTH, HEIGHT}, "Set");
 
-
-    sf::RenderWindow window({1500, 1500}, "Set");
     Vector2d beg{-100, -100};
     Vector2d now;
-    bool resized = false;
+
     sf::Image ff;
     ff.loadFromFile("..\\start.png");
-    std::vector<sf::Image> poses;
-    poses.push_back(ff);
-    std::vector<Vector2d> centers = {{1050, 750}};
-    std::vector<double> ones = {500};
+
+    std::vector poses{ff}; poses.reserve(30);
+    std::vector<Vector2d> centers = {{1050, 750}}; centers.reserve(30);
+    std::vector ones = {WIDTH / 3.}; ones.reserve(30);
+
+
     bool flag = false;
     bool focused = true;
+    bool resized = false;
+
     while (window.isOpen()) {
         auto one = ones.back();
         auto center = centers.back();
+
         sf::Event event{};
         while (window.pollEvent(event)) {
-
             if (event.type == sf::Event::Closed) {
                 window.close();
             } else if(event.type == sf::Event::LostFocus) {
@@ -77,8 +82,8 @@ int main() {
             }
             if (!focused) break;
             if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-                double mX = static_cast<double>(sf::Mouse::getPosition(window).x);
-                double mY = static_cast<double>(sf::Mouse::getPosition(window).y);
+                double mX = (sf::Mouse::getPosition(window).x);
+                double mY = (sf::Mouse::getPosition(window).y);
                 if (beg == Vector2d{-100, -100} || !flag) {
                     beg = Vector2d{mX, mY};
                 }
@@ -95,16 +100,17 @@ int main() {
                 }
                 flag = true;
                 now = beg + Vector2d{std::max(nX, nY), std::max(nX, nY)};
+
             } else if (!sf::Mouse::isButtonPressed(sf::Mouse::Left)
                 && sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
                 if (beg != Vector2d{-100, -100}) {
                     double w = ((now - beg) * 2).x;
                     center -= (beg - (now - beg));
-                    center.x *= 1500;
+                    center.x *= WIDTH;
                     center.x /= w;
-                    center.y *= 1500;
+                    center.y *= WIDTH;
                     center.y /= w;
-                    one *= 1500;
+                    one *= WIDTH;
                     one /= w;
                     beg = {-100, -100};
                     flag = false;
@@ -122,19 +128,19 @@ int main() {
                 }
                 } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
                     flag = false;
-                    center.y += 100;
+                    center.y += HEIGHT / 15.;
                     resized = true;
                 } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
                     flag = false;
-                    center.y -= 100;
+                    center.y -= HEIGHT / 15.;
                     resized = true;
                 } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
                     flag = false;
-                    center.x += 100;
+                    center.x += WIDTH / 15.;
                     resized = true;
                 } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
                     flag = false;
-                    center.x -= 100;
+                    center.x -= HEIGHT / 15.;
                     resized = true;
                 } else {
                     flag = false;
@@ -143,21 +149,21 @@ int main() {
 
         window.clear();
         if (resized) {
-            ff.create(1500, 1500);
-
+            ff.create(WIDTH, HEIGHT);
             std::vector<std::thread> threads;
-            auto partHeight = 1500 / numThreads;
+
+            auto partHeight = WIDTH / numThreads;
             for (auto i = 0ull; i < numThreads; ++i) {
                 auto startY = i * partHeight;
                 auto heightToProcess = (i == numThreads - 1) ? (1500 - startY) : partHeight;
                 threads.emplace_back(generatePart,
-                    std::ref(ff), startY, heightToProcess, 1500, one, center);
-
+                    std::ref(ff), startY, heightToProcess, one, center);
             }
 
             for (auto& thread : threads) {
                 thread.join();
             }
+
             poses.push_back(ff);
             centers.push_back(center);
             ones.push_back(one);
@@ -183,7 +189,6 @@ int main() {
             a.setOutlineThickness(3);
             window.draw(a);
         }
-        // window.draw(c);
         window.display();
     }
 }
